753627872caf4c3a6e860db911aaab5a0a15435d
rehan99000 <rehan99000@gmail.com>
2020-06-25 02:56:09 +0500

    added heroku related changes

--- ambProject/settings.py
+++ ambProject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_heroku
 
 import os
 from datetime import timedelta
@@ -16,6 +17,8 @@ from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Heroku configuration
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
