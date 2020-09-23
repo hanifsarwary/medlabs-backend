5303bd638e2065b0648e43d1af58a7f10d0d14c1
hanifsarwary <hanifsarwari@addo.ai>
2020-09-23 13:23:47 +0500

    static folder added

--- ambProject/settings.py
+++ ambProject/settings.py
@@ -201,6 +201,7 @@ SWAGGER_SETTINGS = {
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # CORS configuration
 CORS_ORIGIN_WHITELIST = (
