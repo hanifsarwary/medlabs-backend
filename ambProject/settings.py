fb98266ab462873a1434e974f136cf242d10bc14
hanifsarwary <hanifsarwari@addo.ai>
2020-09-30 15:27:54 +0500

    new email password added

--- ambProject/settings.py
+++ ambProject/settings.py
@@ -150,9 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'medscreenlabs@gmail.com'
-EMAIL_HOST_PASSWORD = 'medscreenlabsadmin'
-
+EMAIL_HOST_USER = 'appointments@medscreenlabs.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
