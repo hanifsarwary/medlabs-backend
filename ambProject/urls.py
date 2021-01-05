363ffa4cbe3e7dc001a2027a40c87668d571465e
hanifsarwary <l154368@lhr.nu.edu.pk>
2021-01-05 19:53:06 +0500

    convert dollars into cents

--- ambProject/urls.py
+++ ambProject/urls.py
@@ -54,7 +54,7 @@ urlpatterns = [
 
     # jwt token urls
     url(r'^api/token/', UserTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url(r'^api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^api/refresh/token/', TokenRefreshView.as_view(), name='token_refresh'),
     url(r'^api/verify/token/', TokenVerifyView.as_view(), name='token_verify'),
     url(r'^api/password_reset/validate_token/', reset_password_validate_token, name="reset-password-validate"),
     url(r'^api/password_reset/confirm/', ResetPasswordConfirm.as_view(), name="reset-password-confirm"),
